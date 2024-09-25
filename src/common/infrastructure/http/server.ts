62725c9b9dc515b7e1a5cb7baa6548d62f71287d
Ruan Basilio Sobral dos Santos <ruanbssantos@gmail.com>
2024-09-25 02:27:42 -0300

    Configurando typeorm pg

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,9 +1,13 @@
 import { env } from "../env";
+import { dataSource } from "../typeorm";
 import { app } from "./app";
 
+dataSource.initialize().then(() => {
+	app.listen(env?.PORT,()=>{
+		console.log(`Servidor online na porta ${env?.PORT}!`);
+		console.log('Api docs available at Get /docs');
+	});
 
-
-app.listen(env?.PORT,()=>{
-	console.log(`Servidor online na porta ${env?.PORT}!`);
-	console.log('Api docs available at Get /docs');
+}).catch(error => {
+    console.error('Error initializing data source:', error)
 });
