ed054264d73880514a26a20b6db525d4cd48eb3d
Ruan Basilio Sobral dos Santos <ruanbssantos@gmail.com>
2024-09-25 01:47:44 -0300

    swagger

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -2,15 +2,30 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 import { errorHandler } from './middlewares/errorHandler';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express'
 
+
+const  options  =  {
+	definition : {
+	  openapi : '3.0.0' ,
+	  info : {
+		title : 'API Documentation' ,
+		version : '1.0.0' ,
+	  } ,
+	} ,
+	apis : [] ,  // arquivos contendo anotações como acima
+} ;
+const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(routes);
-//app.use(errorHandler);
+app.use(errorHandler);
 
 // app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 // 	if (err instanceof Error){
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -5,4 +5,5 @@ import { app } from "./app";
 
 app.listen(env?.PORT,()=>{
 	console.log(`Servidor online na porta ${env?.PORT}!`);
+	console.log('Api docs available at Get /docs');
 });
